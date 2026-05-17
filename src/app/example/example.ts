3ee9ed41ce8b41446009fde0bf7c24990c6c3fd9
Mawiusfp <kevincarpetano@proton.me>
2026-05-17 12:02:35 +0200

    Añadido no poder sumar numeros negativos

--- src/app/example/example.ts
+++ src/app/example/example.ts
@@ -11,9 +11,13 @@ import { FormsModule } from '@angular/forms';
 export class Example {
   n1: number = 0;
   n2: number = 0;
-  result: number = 0;
+  result: any = 0;
 
   addNumbers() {
-    this.result = this.n1 + this.n2;
+    if(this.n1 < 0 || this.n2 < 0) {
+      this.result = "No queremos números negativos";
+    } else {
+      this.result = String(this.n1 + this.n2);
+    }
   }
 }
