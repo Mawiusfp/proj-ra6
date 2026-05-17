e2f53d5cbcdb576bb495c2939b39067660f989dc
Mawiusfp <kevincarpetano@proton.me>
2026-05-17 12:00:39 +0200

    Añadido formulario para sumar 2 numeros

--- src/app/example/example.ts
+++ src/app/example/example.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-example',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './example.html',
   styleUrl: './example.css',
 })
-export class Example {}
+export class Example {
+  n1: number = 0;
+  n2: number = 0;
+  result: number = 0;
+
+  addNumbers() {
+    this.result = this.n1 + this.n2;
+  }
+}
